a86a2fb864dbc022f6c2451a7c1f04a25eb38414
ArcRoot <phg5253@naver.com>
2020-05-16 21:30:06 +0900

    upload

---
+++ Assets/Scripts/crash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("들어간 물체는 other");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("나간 물체는 other");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("안에 있는 물체는 other");
+    }
+}
